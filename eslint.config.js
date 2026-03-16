527acd5537769bc6e49c19fb0e504133f6aa09f9
Ishrat0628 <mdrazamakandar1@gmail.com>
2026-03-16 13:47:31 +0530

    commited by raza

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/define-config'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -11,7 +11,7 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
